ebd4c22527bde48ab41b1fb1a59d90e8bbc44185
Valentín Ottaviano <valentinottaviano@gmail.com>
2024-03-11 12:54:18 -0300

    Updated with new files

--- updateweb.sh
+++ updateweb.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
-# Source directory
-source_dir="/home/valentinottaviano/Documents/Dropbox/Collage-Notes"
-
-# Destination directory
-destination_dir="/home/valentinottaviano/Documents/quartz/content"
-
-# Use rsync to copy files from the source to the destination
-rsync -av --delete "$source_dir/" "$destination_dir/"
-
-# Explanation:
-# - -a: Archive mode, which preserves permissions and other attributes.
-# - -v: Verbose mode, so you can see what's being copied.
-# - --delete: This option deletes any files in the destination directory that are not in the source directory.
-
-echo "Files copied successfully."
 
 # Change to the destination directory
-cd "$destination_dir"
-cd ..
+
+cd /home/winston/Documents/quartz/
 # Add all changes to Git
 git add .
 
 # Commit the changes with a message
-git commit -m "Updated with new files from Collage-Notes"
+git commit -m "Updated with new files"
 
 # Push the changes to the GitHub repository
 git push
